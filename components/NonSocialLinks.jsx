f2fe5d0b98e609af75f05efad4ef9a52fc78bfc1
Deerg <deerg666@gmail.com>
2023-04-25 19:31:16 -0400

    spaces deleted

--- components/NonSocialLinks.jsx
+++ components/NonSocialLinks.jsx
@@ -1,10 +1,10 @@
 
 const NonSocialLinks = () => {
     return <header className="mx-auto row justify-between items-center" style={{width:390,marginTop:66}}>
-        <button className="button">LINK 1</button>
-        <button className="button" style={{marginTop:35}}>LINK 2</button>
-        <button className="button" style={{marginTop:35}}>LINK 3</button>
-        <button className="button" style={{marginTop:35, marginBottom:35}}>LINK 4</button>
+        <button className="button">LINK1</button>
+        <button className="button" style={{marginTop:35}}>LINK2</button>
+        <button className="button" style={{marginTop:35}}>LINK3</button>
+        <button className="button" style={{marginTop:35, marginBottom:35}}>LINK4</button>
     </header>
 }
 
